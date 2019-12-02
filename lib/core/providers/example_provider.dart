952d2fb58cd372623b908819477e07291df26a82
Julien Grand-Chavin <hello@juliengrandchavin.dev>
2019-12-02 13:53:48 +0100

    Add example

---
+++ lib/core/providers/example_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_provider_architecture_starter/core/providers/base_provider.dart';
+
+class ExampleProvider extends BaseProvider {
+  ExampleProvider() : super();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider_architecture_starter/core/viewmodels/screens/splash_screen_view_model.dart';
+import 'package:flutter_provider_architecture_starter/ui/shared/view_model_provider.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ViewModelProvider<SplashScreenViewModel>(
+      model: SplashScreenViewModel(context: context),
+      builder: (SplashScreenViewModel model) {
+        return Scaffold();
+      },
+    );
   }
 }
